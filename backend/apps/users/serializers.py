0b0f2fe85e5aee3715a1a210ff2326a63541038f
wasim-faris <waseemfaris179@gmail.com>
2026-05-11 15:27:34 +0530

    feat: add user serializers for register, login, view and update

---
+++ backend/apps/users/serializers.py
@@ -0,0 +1,90 @@
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from .models import User,Profile
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password =  serializers.CharField(
+        write_only = True,
+        required = True,
+        validators = [validate_password]
+    )
+    
+    confirm_password = serializers.CharField(
+        write_only = True,
+        required = True
+    )
+    
+    class Meta:
+        model = User,
+        fields = [
+            'email',
+            'name',
+            'password',
+            'confirm_password',
+            'city',
+            'language',
+        ]
+        
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({"password": "Password doesnt match !"})
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('confirm_passoword')
+        #frontend come with dict then **using this one assign in to the variable to save in the db
+        user = User.objects.create_user(**validated_data)
+        Profile.objects.create(user=user)
+        return user
+    
+class LoginSerializer(serializers.Serializer):
+    #used serializer for the taking data not adding to the db 
+    email = serializers.CharField(
+        write_only = True,
+        required = True
+    )
+    password = serializers.CharField(
+        write_only = True,
+        required = True
+    )
+    
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = [
+            'avg_rating',
+            'total_sessions',
+            'credits',
+            'is_verifed',  
+        ]
+        
+        #used to only show in the frontend they cant edit this one          
+        read_only_fields = fields
+
+class UserSerializer(serializers.ModelSerializer):
+    #this is used to take the data with the user objects instead of calling the profile alone using user serializer can get both and show it
+    Profile = ProfileSerializer(read_only = True)
+    
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'email',
+            'name',
+            'city',
+            'language',
+            'photo',
+            'bio',
+            'profile'
+        ]
+        read_only_fields = ['id', 'email']
+    
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = [
+            'name',
+            'city',
+            'language',
+            'photo',
+            'bio'
+        ]
