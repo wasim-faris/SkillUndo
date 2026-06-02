0d3319b29a33b95dc1d0537e2fca88008d21d76b
wasim-faris <waseemfaris179@gmail.com>
2026-06-02 17:48:12 +0530

    feat(chat): initialize chat app and message model

--- backend/apps/chat/models.py
+++ backend/apps/chat/models.py
@@ -1,1 +1,27 @@
 # Create your models here.
+from django.db import models
+from apps.users.models import User
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="sent_messages"
+    )
+
+    receiver = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="received_messages"
+    )
+
+    content = models.TextField()
+
+    is_read = models.BooleanField(
+        default=False
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
